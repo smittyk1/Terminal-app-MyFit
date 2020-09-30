9e5369ea7d55206e87efea4b7d2b02798d1f7b8d
smittyk1 <smittyfg2d@gmail.com>
2020-09-30 21:20:02 +1000

    Added lowcal and high cal tables to nutrition feature

--- src/MyFit.rb
+++ src/MyFit.rb
@@ -18,10 +18,10 @@ prompt = TTY::Prompt.new
 # Arrays used to populate the exercise tables, divided based on muscle group
 chest_ex = [
     ["Pushup", "12 - 15", " 1. Don't allow lower back to dip, keep back straight \n 2. Keep elbows tucked in, don't flair them out \n 3. Slowly bring yourself down with control \n 4. Pause with 1cm between your chest and the ground, hold 5 seconds \n 5. Explode up, locking out your elbows at the top of the motion", "Too hard? \n Use an elevated surface or do it from your knees \nToo easy? \n Wrap a resistance band around your back \n to add force" ],
-    ["Decline \nPushup", "10 - 15", " 1. Keep pushup technique in mind \n 2. Align hands with center of chest \n 3. Keep hands slightly wider than shoulder-width apart \n 4. Keep legs straight, feet together and core tight throughout movement", "Too hard? \n Reduce leg elevation or regress to pushup \nToo easy? \n Elevate hands for deeper range of motion"],
+    ["Decline\n Pushup", "10 - 15", " 1. Keep pushup technique in mind \n 2. Align hands with center of chest \n 3. Keep hands slightly wider than shoulder-width apart \n 4. Keep legs straight, feet together and core tight throughout movement", "Too hard? \n Reduce leg elevation or regress to pushup \nToo easy? \n Elevate hands for deeper range of motion"],
     ["Chest\n Dip", "10 - 12", " 1. Hold onto parallel bars with feet off the ground/elbows locked out \n 2. Lean forward to put emphasis on chest \n 3. Maintain lean whilst lowering yourself \n 4. Keep elbows tucked in throughout motion \n 5. Hold at bottom for a second then strictly push till elbows lockout", "Too hard?\n Place something for your feet to rest on\n whilst you do the movement\nToo easy? \n Invest in a weight vest! Or place a resistance\n band around the back of your neck"],
     ["Archer\nPushup", "5 - 8\nEach arm", " 1. Get into a pushup position but with hands wide apart \n 2. Ensure hands are facing outwards(fingers pointing out, not forward) \n 3. Lower yourself by bending only one arm \n 4. Keep opposite arm straight out (not allowing hand to move/slide) \n 5. Alternate arm each rep until completion", "Too hard?\n instead, elevate one hand slightly\n and conduct a normal pushup with more\n tension placed on lower arm\nToo easy?\n attempt a single-arm pushup"],
-    ["Type-\nwriter\nPushup", "8 - 10", " 1. Start in a wide pushup position with hands facing out \n 2. Lower yourself like any other pushup movement \n 3. At the bottom of the movement, shift your weight onto one arm \n 4. Hold then switch to your other arm (keeping chest close to ground) \n 5. Bring yourself back to the center and push up to complete the rep"],
+    ["Type-\nwriter\nPushup", "8 - 10", " 1. Start in a wide pushup position with hands facing out \n 2. Lower yourself like any other pushup movement \n 3. At the bottom of the movement, shift your weight onto one arm \n 4. Hold then switch to your other arm (keeping chest close to ground) \n 5. Bring yourself back to the center and push up to complete the rep" ],
     [""]
 ]
 
@@ -48,12 +48,27 @@ leg_ex = [
     [],
     []
 ]
+
+lowcal = [
+    ["Greek Yogurt", "60 calories per 100 grams \n Good source of protein"],
+    ["Berries(straw, blue, rasp)", "84 calories per 1 cup \n benefitial fibre and vitamins"],
+    ["Eggs", "70 calories per egg \n reduces hunger by boosting fullness"],
+]
+
+highcal = [
+    [],
+    [],
+    [],
+]
 # styling and titling of various tables
 table_chest = Terminal::Table.new :headings => ['Exercise', 'Rep-range', 'Technique', 'Assistance'], :rows => chest_ex, :title => "Workout \n Select at least 4 exercises from the list below \n Pick at least 2 Green, 1 Yellow and 1 Red", :style => {:border_x => "=", :border_i => "x", :all_separators => true}
 table_back = Terminal::Table.new :headings => ['Exercise', 'Rep-range', 'Technique', 'Assistance'], :rows => back_ex, :title => "Workout \n Select at least 4 exercises from the list below \n Pick at least 2 Green, 1 Yellow and 1 Red", :style => {:border_x => "=", :border_i => "x", :all_separators => true}
 table_arms = Terminal::Table.new :headings => ['Exercise', 'Rep-range', 'Technique', 'Assistance'], :rows => arm_ex, :title => "Workout \n Select at least 4 exercises from the list below \n Pick at least 2 Green, 1 Yellow and 1 Red", :style => {:border_x => "=", :border_i => "x", :all_separators => true}
 table_shoulders = Terminal::Table.new :headings => ['Exercise', 'Rep-range', 'Technique', 'Assistance'], :rows => shoulder_ex, :title => "Workout \n Select at least 4 exercises from the list below \n Pick at least 2 Green, 1 Yellow and 1 Red", :style => {:border_x => "=", :border_i => "x", :all_separators => true} 
 table_legs = Terminal::Table.new :headings => ['Exercise', 'Rep-range', 'Technique', 'Assistance'], :rows => leg_ex, :title => "Workout \n Select at least 4 exercises from the list below \n Pick at least 2 Green, 1 Yellow and 1 Red", :style => {:border_x => "=", :border_i => "x", :all_separators => true}
+table_lowcal = Terminal::Table.new :headings => ['Food', 'Information'], :rows => lowcal, :title => "Low-calorie foods", :style => {:border_x => "=", :border_i => "x", :all_separators => true}
+table_highcal = Terminal::Table.new :headings => ['Food', 'Information'], :rows => highcal, :title => "High-calorie foods", :style => {:border_x => "=", :border_i => "x", :all_separators => true}
+
 
 clear_screen
 puts "
@@ -94,7 +109,7 @@ loop do
                             ██      ██         ██         ██     ██    ██ ██         ██        ██      ██    ██       
                             ███████ ███████    ██    ███████      ██████  ███████    ██        ██      ██    ██    ██                                                                                                                                                                         
  " 
- puts "
+ puts "                                                       (Make a selection with 1 - 4)
                                                                 1. Exercise
                                                                 2. Nutrition
                                                                 3. Motivate 
@@ -277,7 +292,14 @@ when "2"
         end
 
     when "3"
-        puts "THREE"
+        puts table_lowcal
+        puts "When ready, type 'return' to go back to menu"
+        input = gets.chomp
+        if input == "return"
+            next
+        else
+            exit
+        end
     when "4"
         puts "FOUR"
     else
