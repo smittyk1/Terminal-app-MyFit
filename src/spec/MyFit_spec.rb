ac0798c0807fc0905660ff9bd23ea1284a447ed9
smittyk1 <smittyfg2d@gmail.com>
2020-10-04 19:57:24 +1100

    Initial testing attempt 1

--- src/spec/MyFit_spec.rb
+++ src/spec/MyFit_spec.rb
@@ -1,1 +1,14 @@
 require_relative '../MyFit'
+
+
+describe 'array_shuffle' do
+    it 'it should return array.sample' do
+        expect(array_shuffle(array)).to be(array.sample)
+    end
+end
+
+describe 'clear_screen' do
+    it 'it should clear terminal screen' do
+        expect(clear_screen).to be(system('clear'))
+    end
+end
